bb26d826fd77b19b3f46b31de15aaf0a1be573be
emrahcaliskan97 <emrahcaliskan33@outlook.com>
2024-05-07 13:19:51 +0300

    Added Dtos
    Added Dtos

--- Services/Catalog/MultiShop.Catalog/obj/Debug/net6.0/MultiShop.Catalog.AssemblyInfo.cs
+++ Services/Catalog/MultiShop.Catalog/obj/Debug/net6.0/MultiShop.Catalog.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MultiShop.Catalog")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c82c58e4798e15648838dc37cfa3479edec91548")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+925a3e8654e9cfd80388fbfdf89eab9066c7633e")]
 [assembly: System.Reflection.AssemblyProductAttribute("MultiShop.Catalog")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MultiShop.Catalog")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
