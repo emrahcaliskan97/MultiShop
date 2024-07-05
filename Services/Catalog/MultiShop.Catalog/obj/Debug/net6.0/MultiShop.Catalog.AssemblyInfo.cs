3940dd5b8cca79f2c68d6fd70ec2e9796a2b92a5
emrahcaliskan97 <emrahcaliskan33@outlook.com>
2024-07-05 15:33:24 +0300

    Discount Servis oluşturuldu
    Discount Servis oluşturuldu

--- Services/Catalog/MultiShop.Catalog/obj/Debug/net6.0/MultiShop.Catalog.AssemblyInfo.cs
+++ Services/Catalog/MultiShop.Catalog/obj/Debug/net6.0/MultiShop.Catalog.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MultiShop.Catalog")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc15a2176e72931cff92bc3704b6dddfc923306b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c19426d24beb762e7326ba320ea7b6cc134389d5")]
 [assembly: System.Reflection.AssemblyProductAttribute("MultiShop.Catalog")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MultiShop.Catalog")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
